bb494135bee97284b0fd11845ed5319574aaf909
cristian596 <camilithopks6@gmail.com>
2026-06-09 19:41:59 -0500

    se agrego la carta de cocteles

--- src/pages/CartaBebidas.jsx
+++ src/pages/CartaBebidas.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 
 const CartaBebidas = () => {
   return (
-    <div>
-      <h1>carta bebidas</h1>
+    <>
+    <div className='grid lg:grid-cols-3 bg-linear-to-br from-black/80 to-gray-400'>
+      <div className='relative'>
+        <h1>Imagen grande y titulo del menu</h1>
+        <h1>Imagen grande y titulo del menu</h1>
+        <h1>Imagen grande y titulo del menu</h1>
+        <h1>Imagen grande y titulo del menu</h1>
+        <h1>Imagen grande y titulo del menu</h1>
+        <h1>Imagen grande y titulo del menu</h1>
+        <h1>Imagen grande y titulo del menu</h1><h1>Imagen grande y titulo del menu</h1>
+
+        <div className=''>
+          <img src="/Carta/coctels.png" alt="" />
+        </div>
+      </div>
+
+      <div>
+        <h1>Bebidas con alcohol</h1>
+      </div>
+      
+      <div>
+        <h1>Bebidas sin alcohol</h1>
+      </div>
     </div>
+    </>
   )
 }
 
