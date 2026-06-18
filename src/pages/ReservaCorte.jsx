109dff412b98f3574b94ef28b53effa10d289a94
cristian596 <camilithopks6@gmail.com>
2026-06-17 19:39:28 -0500

    Trabajamos en la agenda

--- src/pages/ReservaCorte.jsx
+++ src/pages/ReservaCorte.jsx
@@ -5,8 +5,13 @@ import {colaboradores} from '../data/barberos'
 import { horarios } from '../data/horarios'
 
 const ReservaCorte = () => {
+  const[cliente,setCliente] = useState("")
+  const[correo,setCorreo] = useState("")
   const[servicio,setServicio] = useState("")
-  const [barbero, setBarbero] = useState("")
+  const[barbero, setBarbero] = useState("")
+  const[fecha,setFecha] = useState("")
+  const[hora,setHora] = useState("")
+  
   return (
     <>
     <div className='flex flex-col justify-center items-center'>
@@ -19,85 +24,30 @@ const ReservaCorte = () => {
         </p>
       </div>
       
-      <div className='grid md:grid-cols-3 border'>
-        <div className='md:row-span-2 border p-4'>
+      <div className='py-2'>
+        <div className='  p-4'>
           <div className='flex flex-col'>
             <label htmlFor="">Cliente</label>
-            <input type="text" placeholder='Escribe tu nombre'/>
+            <input onChange={(e)=>setCliente(e.target.value)} type="text" placeholder='Escribe tu nombre' className='w-120 p-1 border'/>
+          </div>
+
+          <div className='flex flex-col'>
+            <label htmlFor="">Correo Electronico</label>
+            <input type="email" required placeholder='Example@gmail.com' className='w-120 p-1 border'/>
           </div>
 
           <div className='flex flex-col'>
             <label htmlFor="">Servicios Masculinos</label>
-            <select 
-            value={servicio}
-            onChange={(e)=> setServicio(e.target.value)}
-            >
-              <option value="">Seleccione el servicio deseado</option>
-              {serviciosMale.map((item,index)=>{
-                return(
-                <option key={index} value={item}>
-                  {item}
-                </option>
-                )
-              })}
-            </select>
-          </div>
-
-          <div className='flex flex-col'>
-            <label htmlFor="">Servicios Femeninos</label>
-            <select 
-            value={servicio}
-            onChange={(e)=> setServicio(e.target.value)}
-            >
-              <option value="">Seleccione el servicio deseado</option>
-              {serviciosFemale.map((item,index)=>{
-                return(
-                  <option value={item} key={index}>
-                    {item}
-                  </option>
-                )
-              })
-  
-              }
-            </select>
-          </div>
-
-          <div className='flex flex-col'>
-            <label htmlFor="">Elige tu Barbero</label>
-            <select 
-            value={barbero}
-            onChange={(e)=> setBarbero(e.target.value)}
-            >
-              <option>Escoje tu barbero preferido</option>
-              {colaboradores.map((barbero)=>{
-                return(
-                  <option value={barbero} key={barbero.id}>
-                    {barbero.nombre}
-                    {barbero.calificacion}
-                  </option>
-                )
-              })
-              }
-            </select>
-          </div>
-
-          <div className='flex flex-col'>
-            <label htmlFor="">Selecciona la fecha</label>
-            <input type="date" />
-          </div>
-
-          <div className=''>
-            <label htmlFor="">Hora de tu servicio</label>
-            <div>
-              {horarios.map((horaSe)=>{
-
+            <div className='flex gap-2'>
+              {serviciosMale.map((serviM)=>{
                 return(
                   <button
                   type='button'
-                  key={horaSe}
-                  className='cursor-pointer'
+                  key={serviM}
+                  onClick={(e)=>setServicio(serviM)}
+                  className={`border p-2 rounded-xl cursor-pointer active:scale-95 duration-300`}
                   >
-                    {horaSe}
+                    {serviM}
                   </button>
                 )
               })
@@ -106,10 +56,93 @@ const ReservaCorte = () => {
             </div>
           </div>
 
-        </div>
+          <div className='flex flex-col'>
+            <label htmlFor="">Servicios Femenino</label>
+            <div className='grid lg:grid-cols-5 gap-2'>
+              {serviciosFemale.map((serviF)=>{
+                return(
+                  <button
+                  type='button'
+                  key={serviF}
+                  onClick={(e)=>setServicio(serviF)}
+                  className={`border p-2 rounded-xl cursor-pointer active:scale-95 duration-300`}
+                  >
+                    {serviF}
+                  </button>
+                )
+              })
 
-        <div>
-asdas
+              }
+            </div>
+          </div>
+
+          <div>
+            {servicio && (
+              <div className='flex flex-col'>
+                <label htmlFor="">Elige tu Barbero</label>
+                <div className='grid grid-cols-6 gap-2'>
+                  {colaboradores.map((barber)=>{
+                    return(
+                      <button
+                      type='button'
+                      key={barber.id}
+                      onClick={()=> setBarbero(barber)}
+                      className='flex flex-col border w-50'
+                      >  
+                      <h3>{barber.nombre}</h3>
+                      <p>{barber.calificacion}</p>
+                      <p>{barber.especialidad}</p>
+                      </button>
+                    )
+                    })
+                  }
+                </div>
+              </div>
+          )}
+          </div>
+          
+          <div>
+            {barbero && (
+              <div>
+                <label htmlFor="">Seleccione la fecha para agendar su servicio</label>
+                <div>
+                  <input 
+                  type="date" 
+                  className='border'
+                  value={fecha}
+                  onChange={(e)=>setFecha(e.target.value)}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div>
+            {fecha && (
+              <div>
+                <label htmlFor="">Seleccione la hora de su servicio</label>
+                <div>
+                  {horarios.map((horaSe)=>{
+                    return(
+                      <button
+                      type='button'
+                      value={horaSe}
+                      onClick={(e)=>setHora(e.target.value)}
+                      >
+                        {horaSe}
+                      </button>
+                    )
+                  })}
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div>
+            {hora && (
+              <button>Agendar Cita!</button>
+            )}
+          </div>
         </div>
       </div>
     </div>
